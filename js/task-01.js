1c6b96fe63b143cb6c4258bb616a6e61098844c5
Viktor Biruk <vikttur.job@gmail.com>
2023-03-22 16:19:08 +0200

    performs task-01

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const listCategoriesRef = document.querySelector('#categories');
+const categoriesRef = listCategoriesRef.querySelectorAll('.item');
+
+console.log('Number of categories: ' + categoriesRef.length);
+
+categoriesRef.forEach(category => {
+	console.log('');
+	console.log('Category: ' + category.querySelector('h2').textContent);
+	console.log('Elements: ' + category.querySelectorAll('li').length);
+});
