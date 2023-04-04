81b0a1a888ed5672ae7b1f2010d3f6390fd8e703
Viktor Biruk <vikttur.job@gmail.com>
2023-04-05 00:47:50 +0300

    performs task-09

--- js/task-09.js
+++ js/task-09.js
@@ -2,4 +2,15 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
+
+const btnChangeColorRef = document.querySelector('.change-color');
+const colorDisplayFieldRef = document.querySelector('.color');
+
+btnChangeColorRef.addEventListener('click', onBtnClick);
+
+function onBtnClick() {
+	const randomColor = getRandomHexColor();
+	document.body.style.backgroundColor = randomColor;
+	colorDisplayFieldRef.textContent = randomColor;
+};
